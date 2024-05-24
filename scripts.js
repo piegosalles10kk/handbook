88d7b9b24737fc37a19ffdc080e72a8c6c0e3355
Diego Salles <104213637+piegosalles10kk@users.noreply.github.com>
2024-05-24 16:14:43 -0300

    Atualização de responsividade

--- scripts.js
+++ scripts.js
@@ -1,1 +1,4 @@
-<script src="./carrosel.js"></script>
+// Função para alternar a classe "active" no menu lateral
+function toggleMenu() {
+    document.querySelector('.side-menu').classList.toggle('active');
+}
